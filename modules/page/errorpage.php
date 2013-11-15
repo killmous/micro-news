a3afa4ad64fbfa425fdde2205757267bb2a293b9
killmous <wcaldwell.email@gmail.com>
2013-11-15 14:33:22 -0600

    First commit to project 	new file:   assets/css/bootstrap-theme.css 	new file:   assets/css/bootstrap-theme.min.css 	new file:   assets/css/bootstrap.css 	new file:   assets/css/bootstrap.min.css 	new file:   assets/fonts/glyphicons-halflings-regular.eot 	new file:   assets/fonts/glyphicons-halflings-regular.svg 	new file:   assets/fonts/glyphicons-halflings-regular.ttf 	new file:   assets/fonts/glyphicons-halflings-regular.woff 	new file:   assets/js/bootstrap.js 	new file:   assets/js/bootstrap.min.js 	new file:   index.php 	new file:   modules/body.php 	new file:   modules/header.php 	new file:   modules/page/errorpage.php 	new file:   modules/page/homepage.php 	new file:   modules/page/navbar.php

---
+++ modules/page/errorpage.php
@@ -0,0 +1,30 @@
+<?php
+
+ob_start();
+
+class ErrorPage
+{
+	var $content;
+
+	function __construct()
+	{
+		$this->content = 
+'
+<div class="container">
+	<div>
+		<h1>404 Error</h1>
+		<h2>Page not found</h2>
+	</div>
+</div>
+';
+	}
+
+	function write()
+	{
+		return $this->content;
+	}
+}
+
+ob_end_flush();
+
+?>
