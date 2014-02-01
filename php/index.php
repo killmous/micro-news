83ad05e2e16493623f309af29bcee3f2ea331d3b
killmous <wcaldwell.email@gmail.com>
2014-02-01 13:16:55 -0600

    Forgot to add the new folder...

---
+++ php/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<?php
+	ob_start();
+	
+	require 'modules/header.php';
+	require 'modules/body.php';
+	
+	$header = new Header();
+	echo $header->write();
+
+	$body = new Body( $_GET );
+	echo $body->write();
+	
+	ob_end_flush();
+	?>
+</html>
